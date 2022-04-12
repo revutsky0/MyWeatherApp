322284ce4cdf90b16a24c29f57da94e7d8d035cc
Andrey <revutsky0@gmail.com>
2022-04-12 09:56:18 +0500

    feat: navigation (in process)

--- app/src/main/java/com/example/myweatherapp/presenter/main/MainViewModel.kt
+++ app/src/main/java/com/example/myweatherapp/presenter/main/MainViewModel.kt
@@ -9,13 +9,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getLastCityUseCase: LoadLastCityUseCase
+
 ) : ViewModel() {
 
-    fun getLastCity(): City? {
-        val lastCity = getLastCityUseCase()
-        Log.d("LAST", "Last city is $lastCity")
-        return lastCity
-    }
+
 
 }
